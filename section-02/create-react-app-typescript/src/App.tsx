2da74885e8ee1843da63394095c7c45ad34a10bc
Victor Testi <victortesti06@gmail.com>
2026-08-02 19:37:18 -0300

    enchantment: create-react-app-typescript

---
+++ section-02/create-react-app-typescript/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Comando para Criação do projeto: <code>npx create-react-app my-app --template typescript</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
