8de6f98c4c58bcc156298f533ffc2e6a45e65896
Victor Testi <victortesti06@gmail.com>
2026-08-02 20:05:22 -0300

    enchantment: Variáveis de Ambiente Personalizados

--- section-02/create-react-app-typescript/src/App.tsx
+++ section-02/create-react-app-typescript/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ENABLE_LOG, ORGANIZATION_NAME } from './settings';
 
 function App() {
+  console.log("REACT_APP_ORGANIZATION_NAME:", process.env.REACT_APP_ORGANIZATION_NAME);
+  console.log("REACT_APP_ENABLE_LOG:", Number(process.env.REACT_APP_ENABLE_LOG));
+  console.log("REACT_APP_RELEASE_DATE:", new Date(process.env.REACT_APP_RELEASE_DATE!));
+
+  if(ENABLE_LOG) {
+    console.log("Nome da Organização:", ORGANIZATION_NAME);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
