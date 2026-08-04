5128b3e92bed1908eb2b5ee54f340df2e4ba75b2
Victor Testi <victortesti06@gmail.com>
2026-08-04 16:37:27 -0300

    enchantment: criação e configuração do react-esbuild-config

---
+++ section-02/react-esbuild-config/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Comandos:</h1>
+            <ul>
+                <li>npm init -y</li>
+                <li>npm install react react-dom</li>
+                <li>npm install --save-dev esbuild</li>
+            </ul>
+        </div>
+    )
+}
+
+export default App
