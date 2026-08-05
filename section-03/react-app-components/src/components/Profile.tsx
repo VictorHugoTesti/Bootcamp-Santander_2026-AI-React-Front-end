94a4b0003b9ec99aa34248f43101d7bef83671b8
Victor Testi <victortesti06@gmail.com>
2026-08-05 15:34:59 -0300

    enchantment: Criando e Estruturando Componentes

---
+++ section-03/react-app-components/src/components/Profile.tsx
@@ -0,0 +1,15 @@
+
+const Profile: React.FC = () => {
+  return (
+    <div>
+      <img
+        src="https://media.licdn.com/dms/image/v2/D5635AQGanBqyw-W9Wg/profile-framedphoto-shrink_400_400/B56ZXUvFabHEAc-/0/1743030868003?e=1786561200&v=beta&t=si24qFgJYPW6kg6mGZuLMmYbKwDaBwsSEPtP7RFJVw4"
+        alt="Victor Hugo"
+      />
+      <h3>Victor Hugo</h3>
+      <p>Desenvolvedor Front-end</p>
+    </div>
+  );
+}
+
+export default Profile;
