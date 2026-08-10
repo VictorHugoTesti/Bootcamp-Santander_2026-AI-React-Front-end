bde0439e24f533f3a4035a8cdf7dc84943044b8d
Victor Testi <victortesti06@gmail.com>
2026-08-10 16:00:03 -0300

    enchantment: Mutação e Benefícios da Pureza

--- section-03/react-app-functions/src/App.tsx
+++ section-03/react-app-functions/src/App.tsx
@@ -7,6 +7,14 @@ interface StepProps {
   instruction: string
 }
 
+const stepsInstructions = [
+  'Criar função do componente',
+  'Exportar e Importar Função Criada',
+  'Finalizar Estrutura',
+  'Adicionar Interações',
+  'Testar Aplicação',
+]
+
 let step = 0;
 
 function Stpe({instruction}: StepProps) {
@@ -19,14 +27,20 @@ function Stpe({instruction}: StepProps) {
 }
 
 function App() {
+  const steps = []
+  for(let i = 1; i <= stepsInstructions.length; i++) {
+    steps.push(<Stpe key={i} step={i} instruction={stepsInstructions[i - 1]}/>)
+  }
+
   return (
     <>
       <h1>Estrutura de um Componente</h1>
       <ul>
-        <Stpe step={1} instruction="Criar função do componente" />
+        {/* <Stpe step={1} instruction="Criar função do componente" />
         <Stpe step={2}instruction="Exportar e Importar Função Criada" />
         <Stpe step={3}instruction="Finalizar Estrutura" />
-        <Stpe step={4}instruction="Adicionar Interações" />
+        <Stpe step={4}instruction="Adicionar Interações" /> */}
+        {steps}
       </ul>
     </>
   );
