96757be8b0bf8ca146e02c06a447913da4b190d6
Victor Testi <victortesti06@gmail.com>
2026-08-10 15:33:27 -0300

    enchantment: Side Effects & Strict Mode

--- section-03/react-app-functions/src/App.tsx
+++ section-03/react-app-functions/src/App.tsx
@@ -1,14 +1,33 @@
 import "./App.css";
-import { Double } from "./components/Double";
-import { Sqare } from "./components/Sqare";
+// import { Double } from "./components/Double";
+// import { Sqare } from "./components/Sqare";
+
+interface StepProps {
+  step: number
+  instruction: string
+}
+
+let step = 0;
+
+function Stpe({instruction}: StepProps) {
+  //Errado: Alterando uma Variável ja existente antes da Função
+  //step = step + 1
+  
+  return (
+    <li>Passo {step}: {instruction}</li>
+  )
+}
 
 function App() {
   return (
     <>
-      <Double number={2} />
-      <Double number={4} />
-      <Sqare number={3} />
-      <Sqare number={5} />
+      <h1>Estrutura de um Componente</h1>
+      <ul>
+        <Stpe step={1} instruction="Criar função do componente" />
+        <Stpe step={2}instruction="Exportar e Importar Função Criada" />
+        <Stpe step={3}instruction="Finalizar Estrutura" />
+        <Stpe step={4}instruction="Adicionar Interações" />
+      </ul>
     </>
   );
 }
