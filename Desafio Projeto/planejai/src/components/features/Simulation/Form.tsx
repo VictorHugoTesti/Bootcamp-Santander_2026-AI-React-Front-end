f78db4514a2f4da23b6afc2562ec7f6bad339d1e
Victor Testi <victortesti06@gmail.com>
2026-08-14 17:36:43 -0300

    enchantment: Aula 12: Configurando os Dados do Formulário Multi-Step

--- Desafio Projeto/planejai/src/components/features/Simulation/Form.tsx
+++ Desafio Projeto/planejai/src/components/features/Simulation/Form.tsx
@@ -1,21 +1,15 @@
-import { PiggyBank } from "lucide-react"
-import { FormStep } from "./FormStep"
-import { StepProgress } from "./Progress"
+import { simulationFormSteps } from '@/data/simulation'
+
+import { FormStep } from './FormStep'
+import { StepProgress } from './Progress'
 
 export const SimulationForm = () => {
-    return (
-        <>
-            <StepProgress currentStep={3} totalSteps={6}/>
-            <FormStep 
-                icon={PiggyBank}
-                title="Renda Mensal Bruta"
-                question="Quanto é depositado na sua conta todo mês (somando todas as fontes)?"
-                inputProps={{
-                    type: 'text',
-                    placeholder: 'ex: 5.000,00',
-                    prefix: 'R$',
-                }}
-            />
-        </>
-    )
+  const currentStep = simulationFormSteps[0]
+
+  return (
+    <>
+      <StepProgress currentStep={1} totalSteps={6} />
+      <FormStep key={currentStep.id} {...currentStep} />
+    </>
+  )
 }
